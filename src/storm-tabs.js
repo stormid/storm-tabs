d26df746767e1d0a0b052389512d3de183028af9
mjbp <mick.perkins@stormid.com>
2016-02-19 15:59:55 +0000

    Adds keyboard support and object compositional structure

--- src/storm-tabs.js
+++ src/storm-tabs.js
@@ -9,126 +9,116 @@
 }(this, function() {
 	'use strict';
     
-    var instances = [],
+    var KEY_CODES = {
+            RETURN: 13,
+            TAB: 9
+        },
+        instances = [],
+        triggerEvents = ['click', 'keydown'],
+        assign = require('object-assign'),
+        merge = require('merge'),
         defaults = {
             titleClass: '.js-tabs__link',
             currentClass: 'active',
             active: 0
         },
-		classlist = require('dom-classlist'),
-		attributelist = require('storm-attributelist'),
-        merge = require('merge'),
-		find = function(node, condition) {
-			var found = [],
-				loop = function(n) {
-					for(var i = 0; i < n.length; i++){
-						!!condition(n[i]) && found.push(n[i]);
-						if(!!n[i].children && !!n[i].children.length) { loop(n[i].children); }
-					}
-					return found;
-				};
-			
-			return (node.children && loop(node.children)) || null;
-		};
-    
-    function StormTabs(el, opts) {
-        this.settings = merge({}, defaults, opts);
-		
-		this.links = [].slice.call(el.querySelectorAll(this.settings.titleClass));
-		
-		this.targets = this.links.map(function(el){
-            return document.getElementById(el.getAttribute('href').substr(1)) || console.error('Tab target not found');
-         });
-		
-        this.current = this.settings.active;
-        this.initAria()
-			.initTitles()
-			.open(this.current);
-    }
-    
-    StormTabs.prototype.initAria = function() {
-        this.links.forEach(function(el, i){
-            attributelist.set(el, {
-                'role' : 'tab',
-                'aria-expanded' : false,
-                'aria-selected' : false,
-                'aria-controls' : this.targets[i].getAttribute('id')
-            });
-        }.bind(this));
-        
-        this.targets.forEach(function(el){
-            attributelist.set(el, {
-                'role' : 'tabpanel',
-                'aria-hidden' : true,
-                'tabIndex': '-1'
-            });
-        });
-        return this;
-    };
-    
-    StormTabs.prototype.initTitles = function() {
-        var handler = function(i){
-            this.toggle(i);
-        };
-        
-        this.links.forEach(function(el, i){
-            el.addEventListener('click', function(e){
-                e.preventDefault();
-                handler.call(this, i);
-            }.bind(this), false);
-        }.bind(this));
-        
-        return this;
-    };
-	
-	StormTabs.prototype.change = function(type, i) {
-		var methods = {
-			open: {
-				classlist: 'add',
-				tabIndex: {
-					target: this.targets[i],
-					value: '0'
-				}
-			},
-			close: {
-				classlist: 'remove',
-				tabIndex: {
-					target: this.targets[this.current],
-					value: '-1'
-				}
-			}
-		};
-		
-		classlist(this.links[i])[methods[type].classlist](this.settings.currentClass);
-		classlist(this.targets[i])[methods[type].classlist](this.settings.currentClass);
-        attributelist.toggle(this.targets[i], 'aria-hidden');
-        attributelist.toggle(this.links[i], ['aria-selected', 'aria-expanded']);
-		attributelist.set(methods[type].tabIndex.target, {
-            'tabIndex': methods[type].tabIndex.value
-        });
-	};
-    
-    StormTabs.prototype.open = function(i) {
-		this.change('open', i);
-        this.current = i;
-        return this;
-    };
-    
-    StormTabs.prototype.close = function(i) {
-		this.change('close', i);
-        return this;
-    };
-    
-    StormTabs.prototype.toggle = function(i) {
-		if(this.current === i) { return; }
-        if(this.current === null) { 
-            this.open(i);
-            return this;
-        }
-         this.close(this.current)
-            .open(i);
-        return this;
-    };
+        StormComponentPrototype = {
+            init: function() {
+                this.links = [].slice.call(this.DOMElement.querySelectorAll(this.settings.titleClass));
+                this.targets = this.links.map(function(el){
+                    return document.getElementById(el.getAttribute('href').substr(1)) || console.error('Tab target not found');
+                 });
+
+                this.current = this.settings.active;
+                this.initAria()
+                    .initTitles()
+                    .open(this.current);
+            },
+            initAria: function() {
+                this.links.forEach(function(el, i){
+                    this.attributelist.set(el, {
+                        'role' : 'tab',
+                        'aria-expanded' : false,
+                        'aria-selected' : false,
+                        'aria-controls' : this.targets[i].getAttribute('id')
+                    });
+                }.bind(this));
+
+                this.targets.forEach(function(el){
+                    this.attributelist.set(el, {
+                        'role' : 'tabpanel',
+                        'aria-hidden' : true,
+                        'tabIndex': '-1'
+                    });
+                }.bind(this));
+                return this;
+            },
+            initTitles: function() {
+                var handler = function(i){
+                    this.toggle(i);
+                };
+
+                this.links.forEach(function(el, i){
+                    triggerEvents.forEach(function(ev){
+                        el.addEventListener(ev, function(e){
+                            if(!!e.keyCode && e.keyCode === KEY_CODES.TAB) { return; }
+                            if(!!!e.keyCode || e.keyCode === KEY_CODES.RETURN){
+                                e.preventDefault();
+                                handler.call(this, i);
+                            }
+                        }.bind(this), false);
+                    }.bind(this));
+                    
+                }.bind(this));
+
+                return this;
+            },
+            change: function(type, i) {
+                var methods = {
+                    open: {
+                        classlist: 'add',
+                        tabIndex: {
+                            target: this.targets[i],
+                            value: '0'
+                        }
+                    },
+                    close: {
+                        classlist: 'remove',
+                        tabIndex: {
+                            target: this.targets[this.current],
+                            value: '-1'
+                        }
+                    }
+                };
+
+                this.classlist(this.links[i])[methods[type].classlist](this.settings.currentClass);
+                this.classlist(this.targets[i])[methods[type].classlist](this.settings.currentClass);
+                this.attributelist.toggle(this.targets[i], 'aria-hidden');
+                this.attributelist.toggle(this.links[i], ['aria-selected', 'aria-expanded']);
+                this.attributelist.set(methods[type].tabIndex.target, {
+                    'tabIndex': methods[type].tabIndex.value
+                });
+            },
+            open: function(i) {
+                this.change('open', i);
+                this.current = i;
+                return this;
+            },
+            close: function(i) {
+                this.change('close', i);
+                return this;
+            },
+            toggle: function(i) {
+                if(this.current === i) { return; }
+                    if(this.current === null) { 
+                        this.open(i);
+                        return this;
+                    }
+                     this.close(this.current)
+                        .open(i);
+                    return this;
+                }
+            };
     
     function init(sel, opts) {
         var els = [].slice.call(document.querySelectorAll(sel));
@@ -137,8 +127,16 @@
             throw new Error('Tabs cannot be initialised, no augmentable elements found');
         }
         
-        els.forEach(function(el){
-            instances.push(new StormTabs(el, opts));
+        els.forEach(function(el, i){
+            instances[i] = assign(Object.create(StormComponentPrototype), {
+                DOMElement: el,
+                settings: merge({}, defaults, opts)
+            }, {
+                classlist: require('dom-classlist'),
+                attributelist: require('storm-attributelist')
+            });
+            
+            instances[i].init();
         });
         return instances;
     }
@@ -151,7 +149,7 @@
     function destroy() {
         instances = [];  
     }
-	
+    
 	return {
 		init: init,
         reload: reload,
